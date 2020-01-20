67d5ce24323edec58e14b5fb5fa896d729b3d7e9
Jack Bridger <jacksbridger@gmail.com>
2020-01-20 11:31:50 +0000

    Revert "in the process of getting subscription to work"
    This reverts commit 00e6b92771cd69dcf83b45153ce446d475b7c349.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { GraphQLServer, PubSub } from "graphql-yoga"
 // PubSub, is based on an event emitter. Provides us with publish and asyncIterator functions
-import { Prisma } from "prisma-binding";
 const pubsub = new PubSub();
 // constant used to connect publish with asyncIterator
 const TODOS_CHANGED = "TODOS_CHANGED"
@@ -21,10 +20,6 @@ const db = {
             title: "buy milk"
         }]
 };
-
-const prisma = new Prisma({ typeDefs: "./prisma-to-do/generated/prisma.graphql", endpoint: "http://localhost:4466/" })
-
-
 // The schema for our graphQL operations and our custom types
 const typeDefs = `
 type Query {
@@ -47,44 +42,36 @@ type ToDo {
 const resolvers = {
     Query: {
         // Return all To Dos
-        toDos: (parent, args, { prisma }, info) => {
-            return prisma.query.toDoes({}, info)
+        toDos: (_, __, { db }) => {
+            return db.toDos;
         }
     },
     Mutation: {
-        createToDo: (parent, { title }, { prisma }, info) => {
-            return prisma.mutation.createToDo({
-                data: {
-                    title
-                }
-            })
+        createToDo: (_, { title }) => {
+            const id = createRandomId();
+            const newToDo = { id, title };
+            db.toDos.push(newToDo);
+            // Notify susbscriptions listening to the TODOS_CHANGED channel 
+            // That a to do has changed and sending through that the newToDo as 
+            // the ToDoChanged payload
+            pubsub.publish(TODOS_CHANGED, { ToDoChanged: newToDo });
+            return newToDo;
         }
     },
     Subscription: {
         // Note: "Subscriptions resolvers are not a function, 
         // but an object with subscribe method, that returns AsyncIterable." 
         ToDoChanged: {
-            subscribe: async (parent, args, { prisma }, info) => {
-                // console.log("came into to do subscribe")
-                // console.log("info is ", info.operation.selectionSet.selections[0].selectionSet)
-                console.log("waiting for pris sub")
-
-                const prismaSub = await prisma.subscription.toDo({}, info)
-                console.log("prisma sub is ", prismaSub)
-                return prismaSub
-            },
-            resolve: payload => {
-                console.log("payload is... !", payload)
-                console.log("payload is... !", payload.ToDoChanged.toString())
-
-                return payload
+            subscribe(_, __, { pubsub }) {
+                // Listen for TODOS_CHANGED changed and then forward the provided
+                // ToDoChanged payload to clients who have subscribed to ToDoChanged
+                return pubsub.asyncIterator(TODOS_CHANGED);
             }
         }
     }
 }
 // Creating neew GQL server running on port 4000, we pass in our schema, resolvers and our 
 // pub sub and database so that our resolvers can access them. 
-
-const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub, db, prisma } })
+const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub, db } })
 
 server.start(console.log("gql node server running on local host 4000"))
